4535812e95879a2f10a0f7b2200e07974f17d022
Guilherme <guilhermesaar02@gmail.com>
2025-11-10 14:48:13 -0300

    first commit

---
+++ src/main/java/com/gstech/PlayCommerce/PlayCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.gstech.PlayCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gstech/PlayCommerce/PlayCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstech.PlayCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
